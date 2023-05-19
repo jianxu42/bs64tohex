447c2bdf8742f80ee3adc1c66b0d8c827a5518eb
jianxu42 <jianxu1@microsoft.com>
2023-05-19 23:01:40 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use base64::{engine::general_purpose, Engine as _};
+use std::env;
+use std::error::Error;
+use std::process;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("not enough arguments");
+        process::exit(1)
+    }
+    let base64_string = args[1].clone();
+    let bytes = general_purpose::STANDARD.decode(base64_string).unwrap();
+    let hex_string = hex::encode(bytes);
+    println!("{}", hex_string.to_ascii_uppercase());
+    Ok(())
+}
